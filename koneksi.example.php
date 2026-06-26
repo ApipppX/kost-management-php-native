51414412ccb411625be7dde3116ebfc278c51b1b
ApipppX <kalamwahyudin444@gmail.com>
2026-06-26 21:58:25 +0700

    Menambahkan fitur baru

---
+++ koneksi.example.php
@@ -0,0 +1,9 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "db_manajemen_kost";
+
+define('MIDTRANS_SERVER_KEY', 'YOUR_SERVER_KEY');
+define('MIDTRANS_IS_PRODUCTION', false);
